0a0e90d19fb093d595e47a09fc9226e865e406fe
Alban Huntziger <albanhuntziger@MacBook-Air-de-Alban.local>
2023-01-04 16:07:13 +0100

    Level 1

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-let buttons = document.getElementsByClassName('button')
-console.log(buttons)
+let calcul = [];
+let ecran = document.getElementById('ecrans');
 
-buttons.addEventListener('click', (e) => {
+document.addEventListener('click', (e) => {
     const value = e.target.textContent;
     calculer(value)
 })
 
 const calculer = (value) => {
-    console.log(value)
+    switch (value){
+        case "=" :
+            let calculStr = calcul.toString().replaceAll(',','').replaceAll('x', '*').replaceAll('÷', '/')
+            if(!calculStr.includes("/0")){
+                let result = eval(calculStr)
+                ecran.innerHTML = result
+                calcul = []
+            }else{
+                ecran.innerHTML = 'Division par 0 impossible'
+            }
+            break
+        case "AC" :
+            calcul = []
+            break
+        default : 
+            ecran.innerHTML = value
+            calcul.push(value)
+        }
 }
